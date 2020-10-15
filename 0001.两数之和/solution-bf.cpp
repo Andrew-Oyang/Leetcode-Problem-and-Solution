8f35b80f5cc5ce0f000b62f098baadab1d506815
Andrew-Oyang <49703643+Andrew-Oyang@users.noreply.github.com>
2020-10-15 18:08:07 +0800

    Update solution-bf.cpp
    fix a format error

--- 0001.两数之和/solution-bf.cpp
+++ 0001.两数之和/solution-bf.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-		// Use Brutal force to enumerate all potentials
+	// Use Brutal force to enumerate all potentials
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1;j < nums.size(); j++) {
                 if(nums[i] + nums[j] == target) {
